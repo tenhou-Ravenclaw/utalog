397f3069d8d77476374b90835b41a92fdaa32625
Fujita Ayato <tenhou.0126@gmail.com>
2025-06-06 16:17:19 +0900

    追加: PWA対応の設定

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
+const config = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})({
   /* config options here */
-};
+});
 
-export default nextConfig;
+export default config;
